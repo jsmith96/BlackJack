0b61b1de99adad8245e76ca8ee40baa7bbd06cda
jsmith96 <smithjohnh@gmail.com>
2015-02-24 15:21:25 -0700

    created Class BlackjackPlayer and Class Blackjack

--- cards.py
+++ cards.py
@@ -112,10 +112,62 @@ class Deck(object):
         """Find the number of cards in the deck"""
         return len(self.__cards)
 
+    def reshuffle(self):
+        """Shuffle the discard pile and add to the bottom of the deck"""
+        random.shuffle(self.__drawn)
+        self.__cards += self.__drawn
+        self.__drawn = []
 
 def create_cards():
-    heart = [Card(x, 'h') for x in range(1, 14)]
-    diam  = [Card(x, 'd') for x in range(1, 14)]
-    spade = [Card(x, 's') for x in range(1, 14)]
-    clubs = [Card(x, 'c') for x in range(1, 14)]
-    return heart + diam + spade + clubs
+    all_ranks = range(1, 14)
+    all_suits = ['H', 'D', 'C', 'S']
+    full_deck = [Card(rank, suit) for suit in all_suits for rank in all_ranks]
+    return Deck(full_deck)
+
+class BlackjackPlayer(object):
+    """Create a Blackjack Player"""
+    def __init__(self, h = []):
+        self.__hand = h
+        self.__stay = False
+        self.__bust = False
+
+    def hit(self, deck):
+        """The player takes a hit (is dealt a card)"""
+        self.__hand += deck.draw()
+
+    def stay(self):
+        """The player chooses to stay (receives no more cards this round)"""
+        self.__stay = True
+
+    def busted(self):
+        """blah"""
+        self.__bust = True
+
+    def get_hand(self):
+        """Get the cards from the player's hand"""
+        return self.__hand
+
+    def get_stay(self):
+        """Find if the player choose to stay"""
+        return self.__stay
+
+    def reset(self):
+        """Clears hand to start a new round"""
+        self.__hand = []
+        self.__stay = False
+
+class Blackjack(object):
+    """Starts a game of Blackjack"""
+    def __init__(self,players=1, deck = create_cards()):
+        self.__players = [BlackjackPlayer() for _ in range(players)]
+        self.__deck = deck.shuffle()
+
+    def set_bust(self, player):
+        """Determines if the player is bust"""
+        hand = player.get_hand()
+        ranks = [card.get_rank for card in hand]
+        values = [rank if rank < 11 else 10 for rank in ranks]
+        score = sum(values)
+        if score > 21:
+            player.busted()
+   
